33f79fd5c9237e69e03bf8e78e6b97edf175326f
Sigurros Inga Helgadottir <6140228@RTS-365.dk>
2020-02-11 13:49:34 +0100

    new errors

--- index.js
+++ index.js
@@ -1,19 +1,49 @@
 //----------------------------1 - ReferenceError
 //hello();
 
-//----------------------------2 - TypeError
-// document.querySelector(".haha").addEventListener("click", ()=>{
-//     console.log("hahahahaha");
-// });
 
-//----------------------------3 - ReferenceError
+//----------------------------2 - ReferenceError
 // let foo = 0;
 
 // foo.forEach(fo => {
     
 // });
 
-//----------------------------4 - SyntaxError
+//----------------------------3 - ReferenceError
+// function error(){
+//     let hey = 0;
+// }
+// console.log(hey);
+
+//-----------------------------------------------------------------------------------------------------------------------------------------
+
+//----------------------------1 - TypeError
+// document.querySelector(".haha").addEventListener("click", ()=>{
+//     console.log("hahahahaha");
+// });
+
+//----------------------------2 - TypeError
+// const constant = 557;
+// constant =668;
+
+//----------------------------3 - TypeError
+// document.querySelector(".hihi").addEventListener("click", ()=>{
+//     this.style.color = "#fff";
+// });
+
+//----------------------------4 - TypeError
+// var num=1;
+
+// try{
+//     num.toUpperCase();
+// } 
+// catch(err){     
+//     console.log(err);
+// }
+
+//-----------------------------------------------------------------------------------------------------------------------------------------
+
+//----------------------------1 - SyntaxError
 // fetch('../haha.json')
 //     .then(response => {
 //         return response.json()
@@ -25,8 +55,4 @@
 //         console.error(error);
 //     })
 
-//----------------------------5 - ReferenceError
-// function error(){
-//     let hey = 0;
-// }
-// console.log(hey);
+
