5f30afe78528cd8fa4249b8abd134c8ecea0e7b5
Sigurros Inga Helgadottir <6140228@RTS-365.dk>
2020-02-11 12:23:38 +0100

    errors

---
+++ index.js
@@ -0,0 +1,33 @@
+//----------------------------1
+//hello();
+
+//----------------------------2
+// document.querySelector(".haha").addEventListener("click", ()=>{
+//     console.log("hahahahaha");
+// });
+
+//----------------------------3
+// let foo = 0;
+
+// foo.forEach(fo => {
+//     console.log(fo);
+// });
+
+//----------------------------4
+// fetch('../haha.json')
+//     .then(response => {
+//         return response.json()
+//     })
+//     .then(data => {
+//         console.log(data);
+//     })
+//     .catch(error => {
+//         console.error(error);
+//     })
+
+//----------------------------5
+// function bla(){
+//     console.log("bla");
+// )
+
+//----------------------------6
