dcbfb9b5f0caab464a5e5fe14934cdddb3a61634
Sigurros Inga Helgadottir <6140228@RTS-365.dk>
2020-02-11 12:45:38 +0100

    new error

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
-//----------------------------1
+//----------------------------1 - ReferenceError
 //hello();
 
-//----------------------------2
+//----------------------------2 - TypeError
 // document.querySelector(".haha").addEventListener("click", ()=>{
 //     console.log("hahahahaha");
 // });
 
-//----------------------------3
+//----------------------------3 - ReferenceError
 // let foo = 0;
 
 // foo.forEach(fo => {
     
 // });
 
-//----------------------------4
-fetch('../haha.json')
-    .then(response => {
-        return response.json()
-    })
-    .then(data => {
+//----------------------------4 - SyntaxError
+// fetch('../haha.json')
+//     .then(response => {
+//         return response.json()
+//     })
+//     .then(data => {
 
-    })
-    .catch(error => {
-        console.error(error);
-    })
+//     })
+//     .catch(error => {
+//         console.error(error);
+//     })
+
+//----------------------------5 - ReferenceError
+console.log(hey);
